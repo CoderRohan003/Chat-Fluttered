dcc2fc754d9610ca1df5855e2623722e87b05b99
CoderRohan003 <rohandutta756@gmail.com>
2024-04-09 19:20:54 +0530

    Initial Commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,14 @@
+import 'dart:ffi';
+
 import 'package:chat_app/auth/login_or_register.dart';
+import 'package:chat_app/firebase_options.dart';
 import 'package:chat_app/themes/light_theme.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp( MaterialApp(
     debugShowCheckedModeBanner: false,
     home: LoginOrRegister(),
--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,11 +1,12 @@
+
+import 'package:chat_app/auth/auth_service.dart';
 import 'package:chat_app/components/my_button.dart';
 import 'package:chat_app/components/my_text_field.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 
 class LoginPage extends StatelessWidget {
-  final TextEditingController emailController = TextEditingController();
-  final TextEditingController passwordController = TextEditingController();
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
 
   // declaring tap mechanism to go to register page
   void Function()? onTap;
@@ -13,7 +14,24 @@ class LoginPage extends StatelessWidget {
   LoginPage({super.key , required this.onTap});
 
   // login method
-  void login() {}
+  void login(BuildContext context) async{
+    // auth Service
+    final authService = AuthService();
+
+  // try login
+  try {
+    await authService.signInWithEmailPassword(_emailController.text, _passwordController.text);
+  }
+
+  // catch any error
+  catch (e) {
+    showDialog(
+      context: context, 
+      builder: (context) => AlertDialog(
+        title: Text(e.toString()),
+      ));
+  }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -47,7 +65,7 @@ class LoginPage extends StatelessWidget {
             MyTextField(
               hintText: "Enter Email",
               hide: false,
-              controller: emailController,
+              controller: _emailController,
             ),
 
             const SizedBox(height: 15.0),
@@ -56,13 +74,13 @@ class LoginPage extends StatelessWidget {
             MyTextField(
               hintText: "Enter Password",
               hide: true,
-              controller: passwordController,
+              controller: _passwordController,
             ),
 
             const SizedBox(height: 35.0),
 
             //login button
-            MyButton(text: "Login", onTap: login),
+            MyButton(text: "Login", onTap: () => login(context)),
 
             const SizedBox(height: 35.0),
 
